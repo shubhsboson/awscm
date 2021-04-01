500271e893a887800632b183979150f04dd6a37b
shubhsboson <contact@shubhsboson.com>
2021-04-01 04:52:01 +0000

    Call get_aws_client instead of passing around aws object

--- src/awscm/glue_tools.py
+++ src/awscm/glue_tools.py
@@ -6,22 +6,23 @@ from awscm.aws_client import get_aws_client
 
 
 def monitor_glue_jobs(**kwargs):
-    aws = get_aws_client('glue', **kwargs)
-    job_list = get_job_list()
+    aws = get_aws_client('glue',**kwargs)
+    job_list = get_job_list(**kwargs)
     # print(job_list)            # for debugging
-    job_run_details_list = get_job_run_details(aws, job_list)
+    job_run_details_list = get_job_run_details(job_list, **kwargs)
     # print(job_run_details_list)    # for debugging
     job_status_list = get_job_status(job_run_details_list)
     print(job_status_list)
 
 
-def get_job_list(aws):
+def get_job_list(**kwargs):
     """
     Fetches names of all glue jobs.
     Note:- This is region specific based on aws object.
     Parameter: aws object
     Returns: List of all glue jobs.
     """
+    aws = get_aws_client('glue', **kwargs)
     job_list = []
     jobs = aws.get_jobs()
     for job in jobs['Jobs']:
@@ -29,13 +30,14 @@ def get_job_list(aws):
     return job_list
 
 
-def get_job_run_details(aws, job_list):
+def get_job_run_details(job_list, **kwargs):
     """
     Fetches latest run details of all the glue jobs present in the list.
     Note:- This is region specific based on aws object.
     Parameter: aws object , job_list
     Returns: List of dictionaries of run details for all glue jobs.
     """
+    aws = get_aws_client('glue', **kwargs)
     job_run_details = []
     for job in job_list:
         job_run_details.append(aws.get_job_runs(JobName=job, MaxResults=1))
