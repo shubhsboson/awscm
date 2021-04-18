c2896ee0a94f6b23a6d1ae4aa63d818ded735e58
shubhsboson <contact@shubhsboson.com>
2021-04-18 17:26:41 +0000

    Use aws Session() to support profile argument

--- src/awscm/awstools.py
+++ src/awscm/awstools.py
@@ -1,9 +1,21 @@
-def get_aws_client(servicename, **kwargs):
+def add_profile():
+    pass
+
+def edit_profile():
+    pass
+
+def get_aws_session(**kwargs):
+    import sys
     import boto3
-    if kwargs.get('region'):
-        region = kwargs['region']
-        aws = boto3.client(servicename, region)
-        return aws
-    else:
-        aws = boto3.client('glue')
-        return aws
+    from botocore.exceptions import ProfileNotFound
+    try:
+        aws_session = boto3.session.Session(**kwargs)
+    except ProfileNotFound:
+        print(f"Profile not found.Please specify correct profile")
+        sys.exit(1)
+    return aws_session
+
+def get_aws_client(aws_session, service, region=None, **kwargs):
+    from botocore.exceptions import NoRegionError , NoCredentialsError 
+    aws_client = aws_session.client(service, region, **kwargs)
+    return aws_client
