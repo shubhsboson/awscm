d4d18da7bd028d1c54a5f8fcfd501cbcfb55800e
shubhsboson <shubhsboson@gmail.com>
2022-02-24 00:39:47 +0530

    Reorder tests to increase reusability

--- src/tests/test_awstools.py
+++ src/tests/test_awstools.py
@@ -4,10 +4,8 @@ import pytest
 from awscm.awstools import get_aws_client, get_aws_session
 from botocore.exceptions import ProfileNotFound
 
-
-def test_get_aws_session(monkeypatch):
-    """Tests the get_aws_session function with mocked config and credetails files."""
-
+@pytest.fixture
+def test_setup(monkeypatch):
     def mock_expanduser(path):
         """
         Returns a new path that points to test_data folder.
@@ -24,8 +22,11 @@ def test_get_aws_session(monkeypatch):
     # Ensure stray default session isn't used.
     monkeypatch.setattr("boto3.DEFAULT_SESSION", None)
 
+def test_get_aws_session(test_setup):
+    """Tests the get_aws_session function with mocked config and credetails files."""
+
     # Check if it fails properly for profiles that don't exist
-    with pytest.raises(ProfileNotFound):
+    with pytest.raises(ProfileNotFound):            
         get_aws_session(profile_name="test_profile_which_doesn't_exist")
     
     # Check if it works for a mocked profile
