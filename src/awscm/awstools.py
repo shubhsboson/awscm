3f8f5063c5f72636d1395e41735e774c9d19593f
shubhsboson <shubhsboson@gmail.com>
2022-02-23 20:04:18 +0530

    Improve implementation of get_aws_session

--- src/awscm/awstools.py
+++ src/awscm/awstools.py
@@ -5,14 +5,8 @@ def edit_profile():
     pass
 
 def get_aws_session(**kwargs):
-    import sys
     import boto3
-    from botocore.exceptions import ProfileNotFound
-    try:
-        aws_session = boto3.session.Session(**kwargs)
-    except ProfileNotFound:
-        print(f"Profile not found.Please specify correct profile")
-        sys.exit(1)
+    aws_session = boto3.session.Session(**kwargs)
     return aws_session
 
 def get_aws_client(aws_session, service, region=None, **kwargs):
